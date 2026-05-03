3221996aa4e33e8430106c26058f9ead8c6eda93
Ero14991 <mohamed.farrag14991@gmail.com>
2026-05-03 23:57:13 +0300

    making vectordb componnent

---
+++ src/stores/vectordb/VectorDBFactory.py
@@ -0,0 +1,27 @@
+from .VectorDBEnums import LLMProvider
+from .vectordb import GrokProvider, CoHereProvider
+from ...helpers.config import Settings
+
+class LLMProviderFactory:
+    def __init__(self, config: Settings):
+        self.config = config
+
+    def create(self, provider: str):
+        if provider == LLMProvider.GROK.value:
+            return GrokProvider(
+                api_key = self.config.GROK_API_KEY,
+                api_url = self.config.XAI_API_URL,
+                default_input_max_characters=self.config.INPUT_DEFAULT_MAX_CHARACTERS,
+                default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+            )
+
+        if provider == LLMProvider.COHERE.value:
+            return CoHereProvider(
+                api_key = self.config.COHERE_API_KEY,
+                default_input_max_characters=self.config.INPUT_DEFAULT_MAX_CHARACTERS,
+                default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+            )
+
+        return None
